3c1a697916d2d719293a21e5c4e4a0f9e4edb1e2
Theodoret <agathotheodoret14@gmail.com>
2026-05-10 22:55:15 +0200

    add grey color default on countries list

--- static/js/map.js
+++ static/js/map.js
@@ -4,6 +4,13 @@ let map = null;
 let mapData = null;
 
 
+const COUNTRIES = ['Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina', 'Armenia', 'Australia', 'Austria',
+             'Azerbaijan', 'Brazil', 'Bulgaria', 'Cameroon', 'Chile', 'China', 'Colombia', 'Croatia', 'Cuba',
+             'Cyprus', 'Czech Republic', 'Ecuador', 'Egypt, Arab Rep.', 'Eritrea', 'Ethiopia', 'France', 'Germany',
+             'Ghana', 'Greece', 'India', 'Indonesia', 'Iran, Islamic Rep.', 'Iraq', 'Ireland', 'Italy', 'Japan',
+             'Jordan', 'Kazakhstan', 'Kenya', 'Lebanon', 'Malta', 'Mexico', 'Morocco', 'Pakistan', 'Peru',
+             'Philippines', 'Russian Federation', 'Syrian Arab Republic', 'Tunisia', 'Turkey', 'Ukraine'];
+
 function initMap() {
 
     // loads the world map as topojson
@@ -34,7 +41,13 @@ function initMap() {
             .attr('d', path)
             .attr('stroke', 'black')
             .attr('stroke-width', 0.5)
-            .attr('fill', 'white');
+            .attr('fill', function(d) {
+                if (COUNTRIES.includes(d.properties.admin)) {
+                    return "grey"; // Color for countries in the array
+                } else {
+                    return "white"; // Default color
+                }
+            });
     });
 
 
